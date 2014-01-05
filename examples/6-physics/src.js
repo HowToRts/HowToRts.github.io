ea0659ce362f34365eb5708de6f1fe6f1796d13f
David Leaver <danzel@localhost.geek.nz>
2014-01-05 15:39:10 +1300

    re-add the flocking behaviours. Sort of works but gets slow in tight gaps.

--- examples/6-physics/src.js
+++ examples/6-physics/src.js
@@ -103,8 +103,11 @@ function gameTick(dt) {
 
 		//Work out our behaviours
 		var ff = steeringBehaviourFlowField(agent);
-		var lc = steeringBehaviourLowestCost(agent);
-		agent.forceToApply = ff.Add(lc.Multiply(0.3));
+		var sep = steeringBehaviourSeparation(agent);
+		var alg = steeringBehaviourAlignment(agent);
+		var coh = steeringBehaviourCohesion(agent);
+
+		agent.forceToApply = ff.Add(sep.Multiply(1)).Add(alg.Multiply(0.2)).Add(coh.Multiply(0.1));
 
 		var lengthSquared = agent.forceToApply.LengthSquared();
 		if (lengthSquared > agent.maxForceSquared) {
@@ -117,6 +120,7 @@ function gameTick(dt) {
 		agent = agents[i];
 
 		//Apply the force
+		//console.log(i + ': ' + agent.forceToApply.x + ', ' + agent.forceToApply.y);
 		agent.body.ApplyImpulse(agent.forceToApply.Multiply(dt), agent.position());
 
 		//Calculate our new movement angle TODO: Should probably be done after running step
@@ -163,55 +167,103 @@ function steeringBehaviourFlowField(agent) {
 	return steerTowards(agent, desiredDirection);
 }
 
-function steeringBehaviourLowestCost(agent) {
 
-	//Do nothing if the agent isn't moving
-	if (agent.velocity().LengthSquared() == 0) {
+function steeringBehaviourSeek(agent, dest) {
+
+	if (dest.x == agent.position().x && dest.y == agent.position().y) {
 		return new B2Vec2();
 	}
 
-	//Find our 4 closest neighbours
-	var floor = agent.position().Copy().Floor(); //Top left Coordinate of the 4
-	var f00 = isValid(floor.x, floor.y) ? dijkstraGrid[floor.x][floor.y] : Number.MAX_VALUE;
-	var f01 = isValid(floor.x, floor.y + 1) ? dijkstraGrid[floor.x][floor.y + 1] : Number.MAX_VALUE;
-	var f10 = isValid(floor.x + 1, floor.y) ? dijkstraGrid[floor.x + 1][floor.y] : Number.MAX_VALUE;
-	var f11 = isValid(floor.x + 1, floor.y + 1) ? dijkstraGrid[floor.x + 1][floor.y + 1] : Number.MAX_VALUE;
 
-	//Find the position(s) of the lowest, there may be multiple
-	var minVal = Math.min(f00, f01, f10, f11);
-	var minCoord = [];
+	//Desired change of location
+	var desired = dest.Copy().Subtract(agent.position());
+	//Desired velocity (move there at maximum speed)
+	desired.Multiply(agent.maxSpeed / desired.Length());
+	//The velocity change we want
+	var velocityChange = desired.Subtract(agent.velocity());
+	//Convert to a force
+	return velocityChange.Multiply(agent.maxForce / agent.maxSpeed);
+}
 
-	if (f00 == minVal) {
-		minCoord.push(floor.Copy());
-	}
-	if (f01 == minVal) {
-		minCoord.push(floor.Copy().Add(new B2Vec2(0, 1)));
-	}
-	if (f10 == minVal) {
-		minCoord.push(floor.Copy().Add(new B2Vec2(1, 0)));
-	}
-	if (f11 == minVal) {
-		minCoord.push(floor.Copy().Add(new B2Vec2(1, 1)));
-	}
+function steeringBehaviourSeparation(agent) {
+	var totalForce = new B2Vec2();
+	var neighboursCount = 0;
 
-	//Tie-break by choosing the one we are most aligned with
-	var currentDirection = agent.velocity().Copy();
-	currentDirection.Normalize();
-
-	var desiredDirection;
-	minVal = Number.MAX_VALUE;
-	for (var i = 0; i < minCoord.length; i++) {
-		minCoord[i].Subtract(agent.position()).Normalize();
-		var directionTo = minCoord[i];
-		var lengthSquared = directionTo.Copy().Subtract(currentDirection).LengthSquared();
-		if (lengthSquared < minVal) {
-			minVal = lengthSquared;
-			desiredDirection = directionTo;
+	for (var i = 0; i < agents.length; i++) {
+		var a = agents[i];
+		if (a != agent) {
+			var distance = agent.position().DistanceTo(a.position());
+			if (distance < agent.minSeparation && distance > 0) {
+				//Vector to other agent
+				var pushForce = agent.position().Copy().Subtract(a.position());
+				totalForce.Add(pushForce.Divide(agent.radius));
+				neighboursCount++;
+			}
 		}
 	}
 
-	//Steer towards it
-	return steerTowards(agent, desiredDirection);
+	if (neighboursCount == 0) {
+		return totalForce; //Zero
+	}
+
+	return totalForce.Multiply(agent.maxForce / neighboursCount);
+}
+
+function steeringBehaviourCohesion(agent) {
+	//Start with just our position
+	var centerOfMass = agent.position().Copy();
+	var neighboursCount = 1;
+
+	for (var i = 0; i < agents.length; i++) {
+		var a = agents[i];
+		if (a != agent) {
+			var distance = agent.position().DistanceTo(a.position());
+			if (distance < agent.maxCohesion) {
+				//sum up the position of our neighbours
+				centerOfMass.Add(a.position());
+				neighboursCount++;
+			}
+		}
+	}
+
+	if (neighboursCount == 1) {
+		return new B2Vec2();
+	}
+
+	//Get the average position of ourself and our neighbours
+	centerOfMass.Divide(neighboursCount);
+
+	//seek that position
+	return steeringBehaviourSeek(agent, centerOfMass);
+}
+
+function steeringBehaviourAlignment(agent) {
+	var averageHeading = new B2Vec2();
+	var neighboursCount = 0;
+
+	//for each of our neighbours (including ourself)
+	for (var i = 0; i < agents.length; i++) {
+		var a = agents[i];
+		var distance = agent.position().DistanceTo(a.position());
+		//That are within the max distance and are moving
+		if (distance < agent.maxCohesion && a.velocity().Length() > 0) {
+			//Sum up our headings
+			var head = a.velocity().Copy();
+			head.Normalize();
+			averageHeading.Add(head);
+			neighboursCount++;
+		}
+	}
+
+	if (neighboursCount == 0) {
+		return averageHeading; //Zero
+	}
+
+	//Divide to get the average heading
+	averageHeading.Divide(neighboursCount);
+
+	//Steer towards that heading
+	return steerTowards(agent, averageHeading);
 }
 
 function steerTowards(agent, desiredDirection) {
